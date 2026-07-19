897f3aca222807d878335f16b0431b213e72cf63
Agrim Karmakar <agrimkarmakar500@gmail.com>
2026-07-19 16:32:06 +0530

    Time: 0 ms (100%), Space: 43 MB (69.28%) - LeetHub

---
+++ 1816-truncate-sentence/1816-truncate-sentence.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String truncateSentence(String s, int k) {
+        int sp=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)==' '){
+                sp+=1;
+            }
+            if(sp==k){
+            return s.substring(0,i);
+        }
+        }
+        return s;
+    }
+     
+   
+}
