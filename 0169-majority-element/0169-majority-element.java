ecb8476ef703fba27a290caa53f37b9ea417d332
Agrim Karmakar <agrimkarmakar500@gmail.com>
2026-08-09 22:19:04 +0530

    Time: 2 ms (70.91%), Space: 55.3 MB (70.19%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,17 +1,44 @@
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         for(int i=0;i<nums.length;i++){
+//             int count=1;
+//             for(int j=i+1;j<nums.length;j++){
+//                 if(nums[i]==nums[j]){
+//                     count++;
+//                 }
+//                 // System.out.println(count);
+//             }
+//             if(count>nums.length/2){
+//                 return nums[i];
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
 class Solution {
     public int majorityElement(int[] nums) {
+        int cnt=0;
+        int candidate =0;
         for(int i=0;i<nums.length;i++){
-            int count=1;
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]==nums[j]){
-                    count++;
-                }
-                // System.out.println(count);
-            }
-            if(count>nums.length/2){
-                return nums[i];
+            if(cnt==0){
+                candidate=nums[i];
+                cnt=1;
+            }else if(candidate==nums[i]){
+                cnt++;
+            }else{
+                cnt--;
             }
         }
-        return -1;
+        int majCnt=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==candidate){
+                majCnt++;
+            }
+        }
+        if(majCnt>nums.length/2){
+            return candidate;
+        }
+        return -1;    
     }
 }
