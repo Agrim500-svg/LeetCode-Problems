5ee02f35dc78aae7cd50cd704d33854c07bfacc4
Agrim Karmakar <agrimkarmakar500@gmail.com>
2026-08-26 18:37:37 +0530

    Time: 0 ms (100%), Space: 41.8 MB (89.08%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.java
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.java
@@ -1,9 +1,5 @@
 class Solution {
     public boolean isStrictlyPalindromic(int n) {
-        if(n!=12){
-            return false;
-        }else{
-            return true;
-        }
+        return false;
     }
 }
