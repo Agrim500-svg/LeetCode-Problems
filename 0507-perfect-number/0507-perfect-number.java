117868d6ef24bb2f408858eee2a4d7d4e4ecb509
Agrim Karmakar <agrimkarmakar500@gmail.com>
2026-09-09 16:24:34 +0530

    Time: 2 ms (84.2%), Space: 42.1 MB (62.34%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -1,10 +1,19 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
-        int fac=0;
-        for(int i=1;i<(num/2)+1;i++){
+        int fac=1;
+        // for(int i=1;i<(num/2)+1;i++){
+        //     if(num%i==0){
+        //         fac+=i;
+        //     } 
+        // }
+        if(num<=1){
+            return false;
+        }
+        for(int i=2;i*i<=num;i++){
             if(num%i==0){
                 fac+=i;
-            } 
+                fac+=num/i;
+            }
         }
         if(fac==num){
             return true;
