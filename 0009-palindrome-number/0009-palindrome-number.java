407771d589967cb38e62d6d43d46ea2b3a08ccfd
Agrim Karmakar <agrimkarmakar500@gmail.com>
2026-09-10 10:36:29 +0530

    Time: 16 ms (4.34%), Space: 46.7 MB (5.92%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -5,7 +5,6 @@ class Solution {
         for(int i=str.length()-1;i>=0;i--){
             rev=rev+str.charAt(i);
         }
-        // System.out.println(rev);
         if(str.equals(rev)){
             return true;
         }else{
