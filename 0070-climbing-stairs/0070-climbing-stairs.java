69c2e1b9cf19b0de6e66b2231f012672a9e7c75b
Agrim Karmakar <agrimkarmakar500@gmail.com>
2026-09-15 11:03:50 +0530

    Time: 0 ms (100%), Space: 41.8 MB (86.52%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int climbStairs(int n) {
+        int p1=1;
+        int p2=1;
+        for(int i=2;i<=n;i++){
+            int res = p1+p2;
+            p2=p1;
+            p1=res;
+        }
+        return p1;
+    }
+}
