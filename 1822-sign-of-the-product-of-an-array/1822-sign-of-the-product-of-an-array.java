ecfa4ccffc5570d7f525fe398bbfe351262d515e
Agrim Karmakar <agrimkarmakar500@gmail.com>
2026-09-19 16:06:05 +0530

    Time: 1 ms (27.66%), Space: 45.1 MB (87.91%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
@@ -1,32 +1,17 @@
-// class Solution {
-//     public int arraySign(int[] nums) {
-//         int prd=1;
-//         for(int i=0;i<nums.length;i++){
-//             prd = prd.multiply(BigInteger.valueOf(nums[i]));
-//         }
-//         System.out.println(prd);
-//         return signFunc(prd); 
-//     }
-//     public int signFunc(int x){
-//         if(x>0){
-//             return 1;
-//         }else if(x<0){
-//             return -1;
-//         }else{
-//             return 0;
-//         }
-//     }
-// }
 class Solution {
-    public int arraySign(int[] nums){
-        int sign=1;
+    public int arraySign(int[] nums) {
+        int cnt=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]==0){
                 return 0;
             }else if(nums[i]<0){
-                sign=-sign;
+                cnt++;
             }
         }
-        return sign;
+        if(cnt%2!=0){
+            return -1;
+        }else{
+            return 1;            
+        }
     }
 }
