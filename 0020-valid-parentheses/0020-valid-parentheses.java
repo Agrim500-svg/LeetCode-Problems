d6e4d497092a9f25729514328668a7a9dd8124ce
Agrim Karmakar <agrimkarmakar500@gmail.com>
2026-09-20 18:51:30 +0530

    Time: 4 ms (36.53%), Space: 43.3 MB (57.45%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> map= new HashMap<>();
+        map.put(')','(');
+        map.put('}','{');
+        map.put(']','[');
+        for(char c: s.toCharArray()){
+            if(c=='('||c=='{'||c=='['){
+                stack.push(c);
+            }else{
+                if(stack.isEmpty()) return false;
+                char top=stack.pop();
+                if(top!=map.get(c)){
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
